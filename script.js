3062ad05142a9350c328f05f8b12708fb45eb2f9
Piyush kumar <piyush1304kumar@gmail.com>
2025-09-11 18:38:13 +0530

    added routes for deployement

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const express=require("express");
+const path =require("path");
 const app=express();
+const { fileURLToPath } = require('url');
+const { dirname }= ('path');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 app.use(express.json());
 const cors=require("cors");
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(express.static(__dirname));
+
 const {userRouter}=require("./routes/user");
 const {TodoRouter}=require("./routes/todo");
 const mongoose=require("mongoose");
@@ -10,6 +20,5 @@ mongoose.connect("mongodb+srv://piyush1304kumar:R2AXPWhWTQpV2v3i@cluster0.tl8run
 app.use("/user",userRouter);
 app.use("/todo",TodoRouter);
 console.log("Todo routes mounted");
-app.listen(3000,()=>{
-    console.log("listening in route 3000");
-})
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
